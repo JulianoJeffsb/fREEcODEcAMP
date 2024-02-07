0e5460d9334ac3b02dc3afc2dafeabb8930026ce
JulianoJeffsb <juliano.jeff.sb@gmail.com>
2024-02-07 16:53:07 -0300

    Oop

---
+++ Carrinho-oop/carrinho.js
@@ -0,0 +1,12 @@
+// Obtendo os elementos do DOM
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const dessertCards = document.getElementById("dessert-card-container");
+// Obtendo os botões do DOM
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+// Obtendo os totais do DOM
+const totalNumberOfItems = document.getElementById('total-items');
+const cartSubTotal = document.getElementById('subtotal');
+const cartTaxes = document.getElementById('taxes');
+const cartTotal = document.getElementById('total');
